18f031cd59bfb2f098ba5423be0d55429cc4811d
julia-kraus <j-kraus@gmx.de>
2019-03-24 23:58:49 +0000

    implement normalization y(1)

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -63,6 +63,15 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
    VectorXd z_pred(3);
    z_pred << rho, phi, rho_dot;
    VectorXd y = z - z_pred;
+  
+    // normalize the angle to value between -pi and pi
+    while(y(1) > M_PI){
+      y(1) -= 2 * M_PI;
+    }
+
+    while(y(1) < -M_PI){
+      y(1) += 2 * M_PI;
+    }
    MatrixXd Ht = H_.transpose();
    MatrixXd S = H_ * P_ * Ht + R_;
    MatrixXd Si = S.inverse();
